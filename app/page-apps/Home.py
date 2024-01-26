07c48c2509332ad8f8e456c7096434304fc1c15e
Tony Lin <tonyelin@gmail.com>
2024-01-27 00:17:32 +0800

    Organize app pages

---
+++ app/page-apps/Home.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+# Adjust hyperlink colorscheme
+links = """<style>
+a:link , a:visited{
+color: 3081D0;
+background-color: transparent;
+}
+
+a:hover,  a:active {
+color: forestgreen;
+background-color: transparent;
+}
+"""
