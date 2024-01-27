4c245cee09fa9d3a70eb6d8d4524fdd9744b7498
Tony Lin <tonyelin@gmail.com>
2024-01-27 13:05:49 +0800

    fixed typo

---
+++ app/page-apps/Changelog.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from streamlit_ketcher import st_ketcher
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+st.markdown('# Chnagelog')
+st.markdown('## Molecular Toolkit v0.0.1')
