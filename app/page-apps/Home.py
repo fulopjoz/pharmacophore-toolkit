2f4650ed9abef9312b37450dde16f5f3569cbd3f
Tony Lin <tonyelin@gmail.com>
2024-01-27 13:05:15 +0800

    Update Home

--- app/page-apps/Home.py
+++ app/page-apps/Home.py
@@ -12,3 +12,9 @@ color: forestgreen;
 background-color: transparent;
 }
 """
+
+st.markdown('# Welcome to Molecular Toolkit')
+
+
+# todo modify caption as a fooder (currently only able to use CSS to modify)
+st.caption('Molecular Toolkit Author: [Tony E. Lin](https://github.com/tlint101/molecular-toolkit)')
