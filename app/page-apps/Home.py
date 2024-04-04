b76823db1f02528476b65e2c0678d1683e9f0d20
Tony Lin <tonyelin@gmail.com>
2024-04-04 16:29:14 +0800

    Update texts

--- app/page-apps/Home.py
+++ app/page-apps/Home.py
@@ -13,12 +13,14 @@ background-color: transparent;
 }
 """
 
-st.markdown("# Welcome to Molecular Toolkit")
+st.markdown("# Welcome to The Molecular Toolkit")
 
-st.markdown("A Place to Practice Basic Cheminformatic Tools")
+st.markdown("A Place to Practice Basic Cheminformatic Tools!")
+
+st.markdown('Get started with links in the sidebar.')
 
 
 # todo modify caption as a fooder (currently only able to use CSS to modify)
 st.caption(
-    "Molecular Toolkit Author: [Tony E. Lin](https://github.com/tlint101/molecular-toolkit)"
+    "The Molecular Toolkit is created by [Tony E. Lin](https://github.com/tlint101/molecular-toolkit)"
 )
--- app/page-apps/Molecule-Sketcher.py
+++ app/page-apps/Molecule-Sketcher.py
@@ -12,20 +12,23 @@ from streamlit_extras.add_vertical_space import add_vertical_space
 # https://blog.streamlit.io/introducing-a-chemical-molecule-component-for-your-streamlit-apps/
 
 # Page text
-st.markdown("# Draw Molecular Structures")
+st.markdown("# Molecule Sketcher")
+st.markdown("### Draw Molecular Structures")
 st.write(
-    "This page will allow users to draw chemical structures of their molecules. Users can translate a smiles string"
-    " into a 2D image or vice versa."
+    "Users can translate a SMILES string, draw molecules, or translate a 2D structure into a smiles string."
 )
-add_vertical_space(1)
+# add_vertical_space(1)
 
 (
     col1,
     col2,
 ) = st.columns(2)
 with col1:
-    molecule = st.text_input(label="", placeholder="Input Smiles String")
+    molecule = st.text_input(label="", placeholder="Input SMILES String")
     smile_code = st_ketcher(molecule, height=600)
-    st.markdown(f"Smile string: {smile_code}")
+    st.markdown(f"Structure SMILES string: {smile_code}")
 with col2:
-    st.header("")
+    if smile_code:
+        st.header("Calculate Chemical Properties")
+
+        # Include scripts for Rule of 5
