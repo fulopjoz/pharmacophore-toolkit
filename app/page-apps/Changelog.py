7bab697c06771ff314b6ee701d2bf2e5c649f901
Tony Lin <tonyelin@gmail.com>
2024-12-11 17:20:08 +0800

    delete streamlit app

--- app/page-apps/Changelog.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-from streamlit_ketcher import st_ketcher
-from streamlit_extras.add_vertical_space import add_vertical_space
-
-st.markdown("# Chnagelog")
-st.markdown("## Molecular Toolkit v0.0.1")
