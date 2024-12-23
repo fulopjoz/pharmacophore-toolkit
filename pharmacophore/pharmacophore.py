3575d6cf65ca89f5927aabb173dd6a6a8d660066
Tony Lin <tonyelin@gmail.com>
2024-12-23 15:19:38 +0800

    move output_features() into Pharmacophore class

--- pharmacophore/pharmacophore.py
+++ pharmacophore/pharmacophore.py
@@ -65,51 +65,51 @@ class Pharmacophore:
         return feature_frequencies_df
 
 
-def output_features(savepath: str = None, mol: rdkit.Chem.rdchem.Mol = None, sphere_size: float = 0.5):
-    """
+    def output_features(self, savepath: str = None, mol: rdkit.Chem.rdchem.Mol = None, sphere_size: float = 0.5):
+        """
 
-    :param savepath: str = None
-        Must be a file in .pml format.
-    :param mol: rdkit.Chem.rdchem.Mol = None
-        Input RDKit Mol. 3D conformational form must be generated. Or input must be RDKit Mol read from an sdf file.
-    :param sphere_size: float = 0.5
-        Set size of spheres.
-    :return:
-    """
-    with open(savepath, "w") as f:
-        # todo create class structure?
-        feature_factory = AllChem.BuildFeatureFactory(os.path.join(RDConfig.RDDataDir, 'BaseFeatures.fdef'))
-        features = feature_factory.GetFeaturesForMol(mol)
-        print(f"Number of features: {len(features)}")
-        for i, feat in enumerate(features):
-            type = feat.GetFamily()
-            if type == "Acceptor":
-                pos = feat.GetPos()  # Feature position
-                f.write(
-                    f"pseudoatom Acceptor_{i}, pos=[{pos.x}, {pos.y}, {pos.z}], color=red\n"
-                )
-            if type == "Donor":
-                pos = feat.GetPos()  # Feature position
-                f.write(
-                    f"pseudoatom Donor_{i}, pos=[{pos.x}, {pos.y}, {pos.z}], color=marine\n"
-                )
-            if type == "Hydrophobe":
-                pos = feat.GetPos()
-                f.write(f"pseudoatom Hydrophobe_{i}, pos=[{pos.x}, {pos.y}, {pos.z}], color=green\n"
-                        )
-            if type == 'Aromatic':
-                pos = feat.GetPos()
-                f.write(f"pseudoatom Aromatic_{i}, pos=[{pos.x}, {pos.y}, {pos.z}], color=pink\n"
-                        )
-            if type == 'LumpedHydrophobe':
-                pos = feat.GetPos()
-                f.write(f"pseudoatom LumpedHydrophobe_{i}, pos=[{pos.x}, {pos.y}, {pos.z}], color=green\n"
-                        )
+        :param savepath: str = None
+            Must be a file in .pml format.
+        :param mol: rdkit.Chem.rdchem.Mol = None
+            Input RDKit Mol. 3D conformational form must be generated. Or input must be RDKit Mol read from an sdf file.
+        :param sphere_size: float = 0.5
+            Set size of spheres.
+        :return:
+        """
+        with open(savepath, "w") as f:
+            # todo create class structure?
+            feature_factory = AllChem.BuildFeatureFactory(os.path.join(RDConfig.RDDataDir, 'BaseFeatures.fdef'))
+            features = feature_factory.GetFeaturesForMol(mol)
+            print(f"Number of features: {len(features)}")
+            for i, feat in enumerate(features):
+                type = feat.GetFamily()
+                if type == "Acceptor":
+                    pos = feat.GetPos()  # Feature position
+                    f.write(
+                        f"pseudoatom Acceptor_{i}, pos=[{pos.x}, {pos.y}, {pos.z}], color=red\n"
+                    )
+                if type == "Donor":
+                    pos = feat.GetPos()  # Feature position
+                    f.write(
+                        f"pseudoatom Donor_{i}, pos=[{pos.x}, {pos.y}, {pos.z}], color=marine\n"
+                    )
+                if type == "Hydrophobe":
+                    pos = feat.GetPos()
+                    f.write(f"pseudoatom Hydrophobe_{i}, pos=[{pos.x}, {pos.y}, {pos.z}], color=green\n"
+                            )
+                if type == 'Aromatic':
+                    pos = feat.GetPos()
+                    f.write(f"pseudoatom Aromatic_{i}, pos=[{pos.x}, {pos.y}, {pos.z}], color=pink\n"
+                            )
+                if type == 'LumpedHydrophobe':
+                    pos = feat.GetPos()
+                    f.write(f"pseudoatom LumpedHydrophobe_{i}, pos=[{pos.x}, {pos.y}, {pos.z}], color=green\n"
+                            )
 
-        f.write("show spheres, Acceptor_*\n")
-        f.write("show spheres, Donor_*\n")
-        f.write("show spheres, Hydrophobe_*\n")
-        f.write("show spheres, Aromatic_*\n")
-        f.write("show spheres, LumpedHydrophobe_*\n")
-        f.write(f"set sphere_scale, {sphere_size}\n")  # Adjust sphere size in PyMOL
-    print(f"Feature visualization script written to {savepath}.")
+            f.write("show spheres, Acceptor_*\n")
+            f.write("show spheres, Donor_*\n")
+            f.write("show spheres, Hydrophobe_*\n")
+            f.write("show spheres, Aromatic_*\n")
+            f.write("show spheres, LumpedHydrophobe_*\n")
+            f.write(f"set sphere_scale, {sphere_size}\n")  # Adjust sphere size in PyMOL
+        print(f"Feature visualization script written to {savepath}.")
